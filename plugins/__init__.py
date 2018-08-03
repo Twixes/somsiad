1f722631196b648dc9decd0aad94834b5d0cf9f6
Twixes <dev@twixes.com>
2018-08-03 14:12:48 +0200

    Added copyright and license notices to plugins/__init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,15 @@
-__all__ = ['help_direct', 'autoreply', 'eightball', 'google', 'wikipedia', 'urban_dictionary', 'isitup', 'image_search', 'youtube']
+# Copyright 2018 Twixes
+
+# This file is part of Somsiad - the Polish Discord bot.
+
+# Somsiad is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# Somsiad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with Somsiad.
+# If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ['help_direct', 'autoreply', 'eightball', 'google', 'wikipedia', 'urban_dictionary', 'isitup', 'image_search',
+    'youtube']
