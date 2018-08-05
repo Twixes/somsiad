e00a8d8d138f99697e84e372bfdf35904cf90b6d
Twixes <dev@twixes.com>
2018-08-06 00:59:51 +0200

    Reorganized the help message

--- plugins/help_direct.py
+++ plugins/help_direct.py
@@ -22,31 +22,32 @@ async def help_direct(ctx):
     em.add_field(name='Dobry!', value='Somsiad jestem. Pomagam w różnych kwestiach, wystarczy mnie zawołać.'
         ' Odpowiadam na wszystkie zawołania z poniższej listy. Pamiętaj tylko zawsze, by zacząć od "'
         f'{conf["command_prefix"]}".\nW nawiasach podane są alternatywne nazwy zawołań - tak dla różnorodności.')
+    em.add_field(name=f'{conf["command_prefix"]}pomocy (pomoc, help)', value='Wysyła użytkownikowi tę wiadomość.',
+        inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}flip', value='Wywraca stół.', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}fix (unflip)', value='Odstawia wywrócony stół na miejsce.',
+        inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}lenny (lennyface)', value=' ( ͡° ͜ʖ ͡°)', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}shrug', value='¯\_(ツ)_/¯', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}ping', value=':ping_pong: Pong!', inline=False)
     em.add_field(name=f'{conf["command_prefix"]}8-ball (8ball, eightball, 8) <pytanie>',
         value='Zadaje <pytanie> magicznej kuli.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}flip', value='Wywraca stół.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}fix (unflip)', value='Odwraca wywrócony stół.', inline=False)
     em.add_field(name=f'{conf["command_prefix"]}gugiel (g) <zapytanie>',
         value='Wysyła <zapytanie> do wyszukiwarki Google i zwraca najlepiej pasujący wynik.', inline=False)
     em.add_field(name=f'{conf["command_prefix"]}img (i) <zapytanie>',
         value='Wysyła <zapytanie> do wyszukiwarki Qwant i zwraca najlepiej pasujący do niego obrazek.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}isitup (isup, up) <url>',
-        value='Za pomocą serwisu isitup.org wykrywa status danej strony.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}pomocy (pomoc, help)', value='Wysyła użytkownikowi tę wiadomość.',
-        inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}lenny (lennyface)', value=' ( ͡° ͜ʖ ͡°)', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}ping', value=':ping_pong: Pong!', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}subreddit (sub, r) <subreddit>',
-        value='Zwraca URL subreddita <subreddit>.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}user (u) <użytkownik>',
-        value='Zwraca URL profilu użytkownika Reddita <użytkownik>.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}shrug', value='¯\_(ツ)_/¯', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}youtube (yt, tuba) <zapytanie>',
+        value='Wysyła <zapytanie> do YouTube i zwraca najlepiej pasujący wynik.', inline=False)
     em.add_field(name=f'{conf["command_prefix"]}wikipediapl (wikipl, wpl) <temat>',
         value='Sprawdza znaczenie <terminu> w polskiej wersji Wikipedii.', inline=False)
     em.add_field(name=f'{conf["command_prefix"]}wikipediaen (wikien, wen) <temat>',
         value='Sprawdza znaczenie <terminu> w anglojęzycznej wersji Wikipedii.', inline=False)
     em.add_field(name=f'{conf["command_prefix"]}urbandictionary (urban) <słowo>',
         value='Sprawdza znaczenie <słowa> w Urban Dictionary.', inline=False)
-    em.add_field(name=f'{conf["command_prefix"]}youtube (yt, tuba) <zapytanie>',
-        value='Wysyła <zapytanie> do YouTube i zwraca najlepiej pasujący wynik.', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}isitup (isup, up) <url>',
+        value='Za pomocą serwisu isitup.org wykrywa status danej strony.', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}subreddit (sub, r) <subreddit>',
+        value='Zwraca URL subreddita <subreddit>.', inline=False)
+    em.add_field(name=f'{conf["command_prefix"]}user (u) <użytkownik Reddita>',
+        value='Zwraca URL profilu użytkownika Reddita <użytkownik Reddita>.', inline=False)
     await ctx.author.send(embed=em)
