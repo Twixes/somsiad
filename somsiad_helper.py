81de4a88fc0fbb61237788f073bc6f1c4c37efe6
Twixes <dev@twixes.com>
2018-08-14 03:18:56 +0200

    Fixed a bug where the conf file lacked newlines
    Also added configuration step numerals

--- somsiad_helper.py
+++ somsiad_helper.py
@@ -21,16 +21,16 @@ brand_color = 0x5370e6
 conf_file_path = os.path.join(os.path.expanduser('~'), '.config', 'somsiad.conf')
 if not os.path.exists(conf_file_path):
     with open(conf_file_path, 'w') as f:
-        f.write('discord_token: ' + str(input('Wprowadź discordowy token bota:\n')))
-        f.write('google_key: ' + str(input('\nWprowadź klucz API Google:\n')))
-        f.write('reddit_id: ' + str(input('\nWprowadź ID aplikacji redditowej:\n')))
-        f.write('reddit_secret: ' + str(input('\nWprowadź szyfr aplikacji redditowej:\n')))
-        f.write('reddit_username: ' + str(input('\nWprowadź redditową nazwę użytkownika:\n')))
-        f.write('reddit_password: ' + str(input('\nWprowadź hasło do konta na Reddicie:\n')))
+        f.write('discord_token: ' + str(input('1. Wprowadź discordowy token bota:\n') + '\n'))
+        f.write('google_key: ' + str(input('2. Wprowadź klucz API Google:\n') + '\n'))
+        f.write('reddit_id: ' + str(input('3. Wprowadź ID aplikacji redditowej:\n') + '\n'))
+        f.write('reddit_secret: ' + str(input('4. Wprowadź szyfr aplikacji redditowej:\n') + '\n'))
+        f.write('reddit_username: ' + str(input('5. Wprowadź redditową nazwę użytkownika:\n') + '\n'))
+        f.write('reddit_password: ' + str(input('6. Wprowadź hasło do konta na Reddicie:\n') + '\n'))
         f.write('user_command_cooldown: ' +
-            str(input('\nWprowadź cooldown między wywołaniami komend przez danego użytkownika (w s):\n')))
-        f.write('command_prefix: ' + str(input('\nWprowadź prefiks komend:\n')))
-        print('\nZapisano konfigurację do ' + conf_file_path)
+            str(input('7. Wprowadź cooldown między wywołaniami komend przez danego użytkownika (w s):\n') + '\n'))
+        f.write('command_prefix: ' + str(input('8. Wprowadź prefiks komend:\n') + '\n'))
+        print(f'Gotowe! Konfigurację zapisano w {conf_file_path}.')
     print('Budzenie Somsiada...')
 
 # If file exists, fetch the keys
