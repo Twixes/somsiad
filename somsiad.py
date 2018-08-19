73c41606253aea2f737513a44768943f471392d2
Twixes <dev@twixes.com>
2018-08-19 22:07:05 +0200

    Extended Informator functionality

--- somsiad.py
+++ somsiad.py
@@ -32,22 +32,62 @@ class Informator:
     noun_variant = staticmethod(lambda number, singular, plural : singular if number == 1 else plural)
 
     @staticmethod
-    def separator(block_major, block_minor, width):
-        """Generates a separator string to the specified length out of given blocks."""
-        block = block_major + block_minor
-        pattern = block * int((width - len(block_major)) / len(block)) + block_major
+    def separator(block, width, verbose=False):
+        """Generates a separator string to the specified length out of given blocks. Can print to the console."""
+        pattern = (block * int(width / len(block)) + block)[:width]
+        if verbose:
+            print(pattern)
         return pattern
 
-    def info(self, lines, verbose=False):
+    def list_of_servers(self, client, verbose=False):
+        """Generates a list of servers the bot is connected to, including the number of days since joining and
+            the number of users. Can print to the console."""
+        servers = sorted(client.guilds, key=lambda server: len(server.members), reverse=True)
+        longest_server_name_length = 0
+        longest_days_since_joining_info_length = 0
+        list_of_servers = ''
+
+        for server in servers:
+            if server.me is not None:
+                if len(server.name) > longest_server_name_length:
+                    longest_server_name_length = len(server.name)
+                date_of_joining = server.me.joined_at.replace(tzinfo=datetime.timezone.utc).astimezone()
+                days_since_joining = (datetime.datetime.now().astimezone() - date_of_joining).days
+                days_since_joining_info = (f'{days_since_joining} '
+                    f'{self.noun_variant(days_since_joining, "dnia", "dni")}')
+                if len(days_since_joining_info) > longest_days_since_joining_info_length:
+                    longest_days_since_joining_info_length = len(days_since_joining_info)
+
+        for server in servers:
+            if server.me is not None:
+                date_of_joining = server.me.joined_at.replace(tzinfo=datetime.timezone.utc).astimezone()
+                days_since_joining = (datetime.datetime.now().astimezone() - date_of_joining).days
+                days_since_joining_info = (f'{days_since_joining} '
+                    f'{self.noun_variant(days_since_joining, "dnia", "dni")}')
+                list_of_servers += (f'{server.name.ljust(longest_server_name_length)} - '
+                    f'od {days_since_joining_info.ljust(longest_days_since_joining_info_length)} - '
+                    f'{str(len(server.members))} '
+                    f'{self.noun_variant(len(server.members), "użytkownik", "użytkowników")}\n')
+        list_of_servers = list_of_servers.strip('\n')
+
+        if verbose:
+            print(list_of_servers)
+
+        return list_of_servers
+
+    def info(self, lines, separator_block=None, verbose=False):
         """Takes a list of lines containing bot information and returns a string ready for printing.
             Can print to the console."""
         info = ''
-        separator = self.separator('==', ' ', os.get_terminal_size()[0])
-        if verbose: print(separator)
+        if separator_block is not None:
+            separator = self.separator(separator_block, os.get_terminal_size()[0])
+            if verbose: print(separator)
         for line in lines:
             info += line + '\n'
-            if verbose: print(line)
-        if verbose: print(separator)
+            if verbose:
+                print(line)
+        if separator_block is not None and verbose:
+            print(separator)
         return info.strip('\n')
 
 # This is what happens every time the bot launches
@@ -58,42 +98,14 @@ def print_info():
     informator = Informator()
 
     number_of_users = len(set(client.get_all_members()))
-    servers = sorted(client.guilds, key=lambda server: len(server.members), reverse=True)
     number_of_servers = len(client.guilds)
-    longest_server_name_length = 0
-    longest_days_since_joining_info_length = 0
-    list_of_servers = ''
-
-    for server in servers:
-        if server.me is not None:
-            if len(server.name) > longest_server_name_length:
-                longest_server_name_length = len(server.name)
-            date_of_joining = server.me.joined_at.replace(tzinfo=datetime.timezone.utc).astimezone()
-            days_since_joining = (datetime.datetime.now().astimezone() - date_of_joining).days
-            days_since_joining_info = (f'{days_since_joining} '
-                f'{informator.noun_variant(days_since_joining, "dnia", "dni")}')
-            if len(days_since_joining_info) > longest_days_since_joining_info_length:
-                longest_days_since_joining_info_length = len(days_since_joining_info)
-
-    for server in servers:
-        if server.me is not None:
-            date_of_joining = server.me.joined_at.replace(tzinfo=datetime.timezone.utc).astimezone()
-            days_since_joining = (datetime.datetime.now().astimezone() - date_of_joining).days
-            days_since_joining_info = (f'{days_since_joining} '
-                f'{informator.noun_variant(days_since_joining, "dnia", "dni")}')
-            list_of_servers += (f'{server.name.ljust(longest_server_name_length)} - '
-                f'od {days_since_joining_info.ljust(longest_days_since_joining_info_length)} - '
-                f'{str(len(server.members))} '
-                f'{informator.noun_variant(number_of_users, "użytkownik", "użytkowników")}\n')
-    list_of_servers = list_of_servers.strip('\n')
-
     info_lines = (
         f'Obudzono Somsiada (ID {str(client.user.id)}).',
         '',
         f'Połączono {informator.with_preposition_variant(number_of_users)} {number_of_users} '
         f'{informator.noun_variant(number_of_users, "użytkownikiem", "użytkownikami")} na {number_of_servers} '
         f'{informator.noun_variant(number_of_servers, "serwerze", "serwerach")}:',
-        list_of_servers,
+        informator.list_of_servers(client),
         '',
         'Link do zaproszenia bota:',
         f'https://discordapp.com/oauth2/authorize?client_id={str(client.user.id)}&scope=bot&permissions=536083543',
@@ -105,7 +117,7 @@ def print_info():
         'Copyright 2018 Habchy, ondondil & Twixes',
     )
 
-    informator.info(info_lines, verbose=True)
+    informator.info(info_lines, '== ', verbose=True)
 
 @client.event
 async def on_ready():
