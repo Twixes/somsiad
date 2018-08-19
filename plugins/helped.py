bc9d50b78ffdb95155389ef7049752dc1d45b8de
Twixes <dev@twixes.com>
2018-08-19 23:08:06 +0200

    Rewritten reacting to facilitate addition of new reaction commands

--- plugins/helped.py
+++ plugins/helped.py
@@ -3,17 +3,14 @@ from discord.ext import commands
 from version import __version__
 from somsiad_helper import *
 
-@client.command(aliases=['pomógł', 'pomogl'])
-@commands.cooldown(1, conf['user_command_cooldown_seconds'], commands.BucketType.user)
-@commands.guild_only()
-async def helped(ctx, *args):
-    """Adds "POMOGL" to the provided user's last non-command message in the form of reactions.
-        If no user was provided, adds "POMOGL" to the last non-command message sent by any non-bot user."""
-    reactions = ('🇵', '🇴', '🇲', '🅾', '🇬', '🇱')
+async def smart_add_reactions(server, channel, args, reactions):
+    """Adds provided emojis to the specified user's last non-command message in the form of reactions.
+        If no user was specified, adds emojis to the last non-command message sent by any non-bot user
+        in the given channel."""
     was_message_found = False
 
     if len(args) == 0:
-        async for message in ctx.channel.history(limit=5):
+        async for message in channel.history(limit=5):
             if (not was_message_found and not message.author.bot and
                 not message.content.startswith(conf['command_prefix'])):
                 for reaction in reactions:
@@ -21,18 +18,18 @@ async def helped(ctx, *args):
                 was_message_found = True
 
     else:
-        if len(args) == 1 and args[0].startswith('<@') and args[0].endswith('>'):
-            commended_user = ctx.guild.get_member(int(args[0].strip('<@!>')))
-        else:
-            commended_user_username = ''
-            for arg in args:
-                commended_user_username += arg + ' '
-            commended_user_username = commended_user_username.strip()
-            commended_user = ctx.guild.get_member_named(commended_user_username)
-
-        async for message in ctx.channel.history(limit=10):
-            if (not was_message_found and message.author == commended_user and
+        async for message in channel.history(limit=10):
+            if (not was_message_found and message.author == get_fellow_server_member(server, args) and
                 not message.content.startswith(conf['command_prefix'])):
                 for reaction in reactions:
                     await message.add_reaction(reaction)
                 was_message_found = True
+
+
+@client.command(aliases=['pomógł', 'pomogl'])
+@commands.cooldown(1, conf['user_command_cooldown_seconds'], commands.BucketType.user)
+@commands.guild_only()
+async def helped(ctx, *args):
+    """Adds "POMOGL" with smart_add_reactions()."""
+    reactions = ('🇵', '🇴', '🇲', '🅾', '🇬', '🇱')
+    smart_add_reactions(ctx.guild, ctx.channel, args, reactions)
