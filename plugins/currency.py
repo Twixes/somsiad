c21e4ef0a263dc3f9281749c6f347b28c3df76f8
Twixes <dev@twixes.com>
2018-08-23 03:45:34 +0200

    Fixed formatting of currency exchange code

--- plugins/currency.py
+++ plugins/currency.py
@@ -23,75 +23,65 @@ from version import __version__
 @commands.cooldown(1, somsiad.conf['user_command_cooldown_seconds'], commands.BucketType.user)
 @commands.guild_only()
 async def currency(ctx, *args):
-    """Provides currency exchange rates."""
-    footer_text1 = 'Dane z CryptoCompare.com (CC BY-NC 3.0)'
-    footer_text2 = 'CryptoCompare.com'
-    footer_icon = 'https://www.cryptocompare.com/media/20562/favicon.png'
+    """Provides (crypto)currency exchange rates."""
+    FOOTER_TEXT = 'CryptoCompare.com (CC BY-NC 3.0)'
+    FOOTER_ICON_URL = 'https://www.cryptocompare.com/media/20562/favicon.png'
+    ERROR_NOTICE = ('Niewłaściwie skonstruowane zapytanie. Zapytanie musi mieć formę "X WALUTA1 w WALUTA2 ' +
+        'WALUTA3 ...", gdzie X to wartość wyrażona w liczbach, a WALUTY to [kody ' +
+        'walut ISO 4217](https://en.wikipedia.org/wiki/ISO_4217#Active_codes) lub kody ' +
+        'kryptowalut. Wartość X oraz fragment WALUTA2 WALUTA3 są opcjonalne.')
+
     if len(args) == 0:
         embed = discord.Embed(title=':warning: Błąd', description=f'Nie podano szukanego hasła!', color=somsiad.color)
-        embed.set_footer(text=footer_text2, icon_url=footer_icon)
-        await ctx.send(embed=embed)
     else:
         query = ' '.join(args)
-        queryRegex = re.compile(r'''
-                                (([0-9]*[.,])?[0-9]+)?      # optional number
-                                (\s)?                       # whitespace
-                                ([a-zA-Z]{2,4})             # 'fr' value
-                                (\s)?                       # whitespace
-                                (to|in|na|do|w)?            # optional preposition
-                                (((\s)?([a-zA-Z]{2,4}))*)   # optional 'to' value
-                                ''', re.VERBOSE)
-
-        mo = queryRegex.search(query)
-        error_description = ('Niewłaściwie skonstruowane zapytanie. Zapytanie musi mieć formę "X WALUTA1 w WALUTA2 ' +
-                             'WALUTA3 ...", gdzie "X" to wartość wyrażona w liczbach, a "WALUTA1" itd. to kody ' +
-                             'zgodne z [ISO 4217](https://en.wikipedia.org/wiki/ISO_4217#Active_codes) lub kody ' +
-                             'kryptowalut. Wartość "X" oraz fragment "w WALUTA2 WALUTA3 ..." są opcjonalne.')
+        query_regex = re.compile(r'''
+            (([0-9]*[.,])?[0-9]+)?      # optional number
+            (\s)?                       # whitespace
+            ([a-zA-Z]{2,4})             # 'initial' value
+            (\s)?                       # whitespace
+            (to|in|na|do|w)?            # optional preposition
+            (((\s)?([a-zA-Z]{2,4}))*)   # optional 'target' value
+            ''', re.VERBOSE)
+        mo = query_regex.search(query)
         if mo is not None:
             if mo.group(1) is not None:
                 num = mo.group(1)
                 num = num.replace(',', '.')
                 num = float(num)
-            fr = mo.group(4).upper()
+            initial = mo.group(4).upper()
             if mo.group(7) != '':
-                to = mo.group(7).upper().lstrip()
-                to = to.replace(' ', ',')
+                target = mo.group(7).upper().lstrip()
+                target = target.replace(' ', ',')
             else:
-                to = 'USD,EUR,PLN'
+                target = 'PLN,USD,EUR'
             headers = {'User-Agent': somsiad.user_agent}
-            crypto_url = ('https://min-api.cryptocompare.com/data/price' +
-                          f'?fsym={fr}&tsyms={to}&extraParams=Somsiad{__version__}')
+            url_safe_user_agent = somsiad.user_agent.replace('/', '%2F')
+            api_url = (f'https://min-api.cryptocompare.com/data/price?fsym={initial}&tsyms={target}'
+                f'&extraParams={url_safe_user_agent}')
             async with aiohttp.ClientSession() as session:
-                async with session.get(crypto_url, headers=headers) as r:
-                    if r.status == 200:
-                        res = await r.json()
-                        if 'Response' in res:
-                            if res['Response'] == 'Error' and res['Type'] == 1:
-                                embed = discord.Embed(title=':warning: Błąd',
-                                                      description=error_description,
-                                                      color=somsiad.color)
-                                embed.set_footer(text=footer_text2, icon_url=footer_icon)
+                async with session.get(api_url, headers=headers) as response:
+                    if response.status == 200:
+                        response_data = await response.json()
+                        if 'Response' in response_data:
+                            if response_data['Response'] == 'Error' and response_data['Type'] == 1:
+                                embed = discord.Embed(title=':warning: Błąd', description=ERROR_NOTICE,
+                                    color=somsiad.color)
                             else:
                                 embed = discord.Embed(title=':warning: Błąd',
-                                                      description='Niewłaściwie skonstruowane zapytanie.',
-                                                      color=somsiad.color)
-                                embed.set_footer(text=footer_text2, icon_url=footer_icon)
+                                    description='Niewłaściwie skonstruowane zapytanie!', color=somsiad.color)
                         else:
                             if 'num' not in locals():
                                 num = 1
-                            currency_values = [(str(num * v) + ' ' + k)
-                                               for k, v in res.items()]
+                            currency_values = [(str(num * value) + ' ' + currency) for currency, value in
+                                response_data.items()]
                             currency_values = '\n'.join(currency_values)
-                            embed = discord.Embed(title=f'{num:.2f} {fr}', description=currency_values,
-                                                  color=somsiad.color)
-                            embed.set_footer(text=footer_text1, icon_url=footer_icon)
-                        await ctx.send(embed=embed)
+                            embed = discord.Embed(title=f'{num} {initial}', description=currency_values,
+                                color=somsiad.color)
                     else:
-                        embed = discord.Embed(title=':warning: Błąd', description='Nie można połączyć się z serwisem.',
-                                              color=somsiad.color)
-                        embed.set_footer(text=footer_text2, icon_url=footer_icon)
-                        await ctx.send(embed=embed)
+                        embed = discord.Embed(title=':warning: Błąd', description='Nie można połączyć się '
+                            'z serwisem, CryptoCompare.com!', color=somsiad.color)
         else:
-            embed = discord.Embed(title=':warning: Błąd', description=error_description, color=somsiad.color)
-            embed.set_footer(text=footer_text2, icon_url=footer_icon)
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=':warning: Błąd', description=ERROR_NOTICE, color=somsiad.color)
+    embed.set_footer(text=FOOTER_TEXT, icon_url=FOOTER_ICON_URL)
+    await ctx.send(embed=embed)
