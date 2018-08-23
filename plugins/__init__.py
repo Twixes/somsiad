70c456c5847a51a6a7874a0f98c4aab633000d91
Twixes <dev@twixes.com>
2018-08-23 02:04:07 +0200

    Renamed the Google search plugin file to avoid conflicts

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,5 +11,5 @@
 # You should have received a copy of the GNU General Public License along with Somsiad.
 # If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ['help_direct', 'autoreply', 'eightball', 'google', 'wikipedia', 'urbandictionary', 'isitup', 'youtube',
-           'reddit_verification', 'currency', 'helped', 'goodreads']
+__all__ = ['help_direct', 'autoreply', 'eightball', 'google_search', 'wikipedia', 'urbandictionary', 'isitup',
+    'youtube', 'reddit_verification', 'currency', 'helped', 'goodreads']
