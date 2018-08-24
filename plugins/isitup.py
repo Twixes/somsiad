2aaaf5cfd188ed2447cc140a3c2652ee032ca1b9
Twixes <dev@twixes.com>
2018-08-24 13:27:08 +0200

    Reworked isitup.org website status checking so that it returns an embed

--- plugins/isitup.py
+++ plugins/isitup.py
@@ -17,41 +17,65 @@ from discord.ext import commands
 from somsiad_helper import *
 
 
-@somsiad.client.command(aliases=['isup', 'up'])
+@somsiad.client.command(aliases=['isup'])
 @commands.cooldown(1, somsiad.conf['user_command_cooldown_seconds'], commands.BucketType.user)
 @commands.guild_only()
 async def isitup(ctx, *args):
     """Returns information about website status."""
+    FOOTER_TEXT = 'Is it up?'
+
     if not args:
-        await ctx.send(f':warning: Musisz podać parametr wyszukiwania, {ctx.author.mention}.')
+        embed = discord.Embed(
+        title=f':warning: Błąd',
+        description='Nie podano adresu strony do sprawdzenia!',
+        color=somsiad.color
+    )
     else:
-        query = ' '.join(args)
+        query = '.'.join(args)
         url = f'https://isitup.org/{query}.json'
-        RESPONSE_CODE_URLS = {
+        RESPONSE_CODE_WIKIPEDIA_URLS = {
             1:'https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP#Kody_informacyjne',
             2:'https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP#Kody_powodzenia',
             3:'https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP#Kody_przekierowania',
             4:'https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP#Kody_błędu_aplikacji_klienta',
-            5:'https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP#Kody_błędu_serwera_HTTP'}
+            5:'https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP#Kody_błędu_serwera_HTTP'
+        }
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as r:
                 if r.status == 200:
                     res = await r.json()
                     # Website online
                     if res['status_code'] == 1:
-                        res_code = RESPONSE_CODE_URLS[int(str(res['response_code'])[0])]
+                        res_code_wikipedia_url = RESPONSE_CODE_WIKIPEDIA_URLS[int(str(res['response_code'])[0])]
                         res_time = res['response_time'] * 1000
-                        description = (f'Strona [{res["domain"]}](http://{res["domain"]}) jest dostępna. '
-                            f'Z IP [{res["response_ip"]}](http://{res["response_ip"]}) otrzymano kod odpowiedzi '
-                            f'[{res["response_code"]}]({res_code}) w czasie {int(res_time)} ms.')
+                        embed = discord.Embed(
+                            title=f':white_check_mark: Strona {res["domain"]} jest dostępna',
+                            url=f'http://{res["domain"]}',
+                            description=f'Z IP [{res["response_ip"]}](http://{res["response_ip"]}) otrzymano '
+                            f'kod odpowiedzi [{res["response_code"]}]({res_code_wikipedia_url}) '
+                            f'w czasie {int(res_time)} ms.',
+                            color=somsiad.color
+                        )
                     # Website offline
                     elif res['status_code'] == 2:
-                        description = (f'Wygląda na to, że strona [{res["domain"]}](http://{res["domain"]}) '
-                            'jest niedostępna!')
+                        embed = discord.Embed(
+                            title=f':red_circle: Strona {res["domain"]} jest niedostępna',
+                            url=f'http://{res["domain"]}',
+                            color=somsiad.color
+                        )
                     # Wrong URL
                     elif res['status_code'] == 3:
-                        description = ('Do wykonania testu potrzebny jest poprawny adres URL. Spróbuj ponownie.')
-                    embed = discord.Embed(title='Is it up?', description=description, color=somsiad.color)
-                    await ctx.send(embed=embed)
+                        embed = discord.Embed(
+                            title=':warning: Błąd',
+                            description='Podany adres jest niepoprawny.',
+                            color=somsiad.color
+                        )
                 else:
-                    await ctx.send(f':warning: Nie można połączyć się z serwisem isitup.org, {ctx.author.mention}')
+                        embed = discord.Embed(
+                            title=':warning: Błąd',
+                            description='Nie udało się połączyć z serwerem sprawdzania statusu stron!',
+                            color=somsiad.color
+                        )
+
+    embed.set_footer(text=FOOTER_TEXT)
+    await ctx.send(ctx.author.mention, embed=embed)
