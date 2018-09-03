ec4c8cef93f14e419dd43d5f9443e6591c058a98
Twixes <dev@twixes.com>
2018-09-03 23:49:39 +0200

    Made loading of individual server databases optional

--- server_settings.py
+++ server_settings.py
@@ -62,7 +62,7 @@ class ServerSettingsManager:
         self.servers['ids'] = [int(server['server_id']) for server in self._servers_db_cursor.fetchall()]
         return self.servers['ids']
 
-    def load_server(self, server_id) -> dict:
+    def load_server(self, server_id: int, load_own_db: bool = False) -> dict:
         """Loads the specified server."""
         if not self.is_server_known(server_id):
             self._servers_db_cursor.execute(
@@ -76,31 +76,32 @@ class ServerSettingsManager:
             (server_id,)
         )
         self.servers[server_id].update(self.dict_from_row(self._servers_db_cursor.fetchone()))
-        server_db_path = os.path.join(somsiad.storage_dir_path, f'server_{server_id}.db')
-        self.servers[server_id]['_db'] = sqlite3.connect(server_db_path)
-        self.servers[server_id]['_db'].row_factory = sqlite3.Row
-        self.servers[server_id]['_db_cursor'] = self.servers[server_id]['_db'].cursor()
-        self.servers[server_id]['_db_cursor'].execute(
-            'PRAGMA foreign_keys = ON'
-        )
-        self.servers[server_id]['_db'].commit()
-        self.servers[server_id]['_db_cursor'].execute(
-            '''SELECT name FROM sqlite_master WHERE type = "table"'''
-        )
-        tables = [table['name'] for table in self.servers[server_id]['_db_cursor'].fetchall()]
-        for table in tables:
-            self.servers[server_id][table] = {}
+        if load_own_db:
+            server_db_path = os.path.join(somsiad.storage_dir_path, f'server_{server_id}.db')
+            self.servers[server_id]['_db'] = sqlite3.connect(server_db_path)
+            self.servers[server_id]['_db'].row_factory = sqlite3.Row
+            self.servers[server_id]['_db_cursor'] = self.servers[server_id]['_db'].cursor()
             self.servers[server_id]['_db_cursor'].execute(
-                f'SELECT * FROM {table}'
+                'PRAGMA foreign_keys = ON'
             )
-            rows = [self.dict_from_row(row) for row in self.servers[server_id]['_db_cursor'].fetchall()]
-            self.servers[server_id][table] = rows
+            self.servers[server_id]['_db'].commit()
+            self.servers[server_id]['_db_cursor'].execute(
+                '''SELECT name FROM sqlite_master WHERE type = "table"'''
+            )
+            tables = [table['name'] for table in self.servers[server_id]['_db_cursor'].fetchall()]
+            for table in tables:
+                self.servers[server_id][table] = {}
+                self.servers[server_id]['_db_cursor'].execute(
+                    f'SELECT * FROM {table}'
+                )
+                rows = [self.dict_from_row(row) for row in self.servers[server_id]['_db_cursor'].fetchall()]
+                self.servers[server_id][table] = rows
         return self.servers[server_id]
 
     def load_all_servers(self) -> dict:
         """Loads all known servers."""
         for server_id in self.load_servers_list():
-            self.load_server(server_id)
+            self.load_server(server_id, load_own_db=True)
         return self.servers
 
     def ensure_table_existence_for_server(self, server_id: int, table_name: str, columns: list):
