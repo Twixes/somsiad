8090ba100fc2ea2ec1c1fa2d59d660e5acaebaed
Twixes <dev@twixes.com>
2018-10-08 21:39:06 +0200

    Enhanced oof statistics

--- plugins/oof.py
+++ plugins/oof.py
@@ -26,18 +26,34 @@ class Oof:
     )
 
     @classmethod
-    def get_oofs(cls, server: discord.Guild, member: Union[discord.User, discord.Member]) -> Union[int, None]:
+    def get_oofs(cls, server: discord.Guild, member: Union[discord.User, discord.Member] = None) -> Union[int, None]:
+        """Returns the number of times the provided user oofed on the provided server.
+        If not provided a user, returns the total number of oofs on the server.
+        """
         server_data_manager.ensure_table_existence_for_server(server.id, cls.TABLE_NAME, cls.TABLE_COLUMNS)
-        server_data_manager.servers[server.id]['db_cursor'].execute(
-            'SELECT oofs FROM oof WHERE user_id = ?',
-            (member.id,)
-        )
-        result = server_data_manager.servers[server.id]['db_cursor'].fetchone()
-        oofs = None if result is None else result['oofs']
+        if member is None:
+            server_data_manager.servers[server.id]['db_cursor'].execute(
+                'SELECT oofs FROM oof',
+            )
+            result = server_data_manager.servers[server.id]['db_cursor'].fetchall()
+            if result:
+                oofs = 0
+                for oofer in result:
+                    oofs += oofer['oofs']
+            else:
+                oofs = None
+        else:
+            server_data_manager.servers[server.id]['db_cursor'].execute(
+                'SELECT oofs FROM oof WHERE user_id = ?',
+                (member.id,)
+            )
+            result = server_data_manager.servers[server.id]['db_cursor'].fetchone()
+            oofs = None if result is None else result['oofs']
         return oofs
 
     @classmethod
-    def get_oofs_ranking(cls, server: discord.Guild) -> List[list]:
+    def get_oofers(cls, server: discord.Guild) -> List[list]:
+        """Returns a list of users who have oofed on the provided server, sorted by the number of oofs, descending."""
         server_data_manager.ensure_table_existence_for_server(server.id, cls.TABLE_NAME, cls.TABLE_COLUMNS)
         server_data_manager.servers[server.id]['db_cursor'].execute(
             'SELECT user_id, oofs FROM oof'
@@ -51,6 +67,7 @@ class Oof:
 
     @classmethod
     def ensure_user_registration(cls, server: discord.Guild, member: Union[discord.User, discord.Member]):
+        """Ensure that the provided user is a registered oofer on the provided server."""
         if cls.get_oofs(server, member) is None:
             server_data_manager.servers[server.id]['db_cursor'].execute(
                 'INSERT INTO oof(user_id) VALUES(?)',
@@ -60,6 +77,7 @@ class Oof:
 
     @classmethod
     def increment(cls, server: discord.Guild, member: Union[discord.User, discord.Member]):
+        """Increments the number of oofs by 1 for the provided user on the provided server."""
         cls.ensure_user_registration(server, member)
         server_data_manager.servers[server.id]['db_cursor'].execute(
             'UPDATE oof SET oofs = oofs + 1 WHERE user_id = ?',
@@ -78,21 +96,19 @@ async def oof(ctx):
     await ctx.send('Oof!')
 
 
-@oof.command(aliases=['ile'])
+@oof.command(aliases=['user', 'member', 'użytkownik', 'członek'])
 @discord.ext.commands.cooldown(
     1, somsiad.conf['command_cooldown_per_user_in_seconds'], discord.ext.commands.BucketType.user
 )
 @discord.ext.commands.guild_only()
-async def how_many(ctx, user: discord.User = None):
-    is_selfcheck = False
+async def oof_user(ctx, user: discord.User = None):
     if user is None:
         user = ctx.author
-        is_selfcheck = True
 
     oofs = Oof.get_oofs(ctx.guild, user)
     oofs = 0 if oofs is None else oofs
 
-    if is_selfcheck:
+    if user == ctx.author:
         embed = discord.Embed(
             title='Masz na koncie '
             f'{TextFormatter.word_number_variant(oofs, "oofnięcie", "oofnięcia", "oofnięć")}',
@@ -108,8 +124,8 @@ async def how_many(ctx, user: discord.User = None):
     await ctx.send(ctx.author.mention, embed=embed)
 
 
-@how_many.error
-async def how_many_error(ctx, error):
+@oof_user.error
+async def oof_user_error(ctx, error):
     if isinstance(error, discord.ext.commands.BadArgument):
         embed = discord.Embed(
             title=':warning: Nie znaleziono podanego użytkownika!',
@@ -118,29 +134,31 @@ async def how_many_error(ctx, error):
         await ctx.send(ctx.author.mention, embed=embed)
 
 
-@oof.command()
+@oof.command(aliases=['server', 'serwer'])
 @discord.ext.commands.cooldown(
     1, somsiad.conf['command_cooldown_per_user_in_seconds'], discord.ext.commands.BucketType.user
 )
 @discord.ext.commands.guild_only()
-async def ranking(ctx):
-    oofs_ranking = Oof.get_oofs_ranking(ctx.guild)
+async def oof_server(ctx):
+    oofers = Oof.get_oofers(ctx.guild)
+    total_oofs = Oof.get_oofs(ctx.guild)
 
     top_oofers = []
     rank = 1
-    for oofer in oofs_ranking[:5]:
+    for oofer in oofers[:5]:
         oofer_user = ctx.bot.get_user(oofer['user_id'])
         top_oofers.append(
             f'{rank}. {oofer_user.mention} – '
             f'{TextFormatter.word_number_variant(oofer["oofs"], "oofnięcie", "oofnięcia", "oofnięć")}'
         )
         rank += 1
-    top_oofers_string = '\n'.join(top_oofers) if top_oofers else 'Brak'
+    top_oofers_string = '\n'.join(top_oofers)
 
     embed = discord.Embed(
-        title=f'Ranking ooferów serwera {ctx.guild}',
-        description=top_oofers_string,
+        title=f'Do tej pory oofnięto na serwerze {TextFormatter.word_number_variant(total_oofs, "raz", "razy")}',
         color=somsiad.color
     )
+    if top_oofers:
+        embed.add_field(name='Najaktywniejsi ooferzy', value=top_oofers_string, inline=False)
 
     await ctx.send(ctx.author.mention, embed=embed)
