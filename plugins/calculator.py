b1393f9fc56eb1542bc202ed50b2945d57449eda
Twixes <dev@twixes.com>
2018-12-01 02:38:14 +0100

    Improved exception handling

--- plugins/calculator.py
+++ plugins/calculator.py
@@ -23,64 +23,70 @@ parser = Parser()
 @discord.ext.commands.cooldown(
     1, somsiad.conf['command_cooldown_per_user_in_seconds'], discord.ext.commands.BucketType.user
 )
-async def calculate(ctx, *args):
+async def calculate(ctx, *, data):
     """Calculates the provided expressions and sends the results.
     If calculation is not possible with available data, simplifies the expression.
     """
-    if args:
-        input_data = ' '.join(args).strip(';').split(';')
-        expression = input_data[0].strip()
-        variables_list = input_data[1:]
-        variables_dict = {}
-        for variable in variables_list:
-            key_value_pair = variable.split('=')
+    input_data = data.strip(';').split(';')
+    expression = input_data[0].strip()
+    variables_list = input_data[1:]
+    variables_dict = {}
+    for variable in variables_list:
+        key_value_pair = variable.split('=')
+        try:
+            variables_dict[key_value_pair[0].strip()] = float(key_value_pair[1].strip())
+        except (IndexError, ValueError):
+            embed = discord.Embed(
+                title=':warning: Podano nieprawidłowe zmienne!',
+                color=somsiad.color
+            )
+            return await ctx.send(ctx.author.mention, embed=embed)
+
+    try:
+        parsed_expression = parser.parse(expression)
+    except Exception:
+        embed = discord.Embed(
+            title=':warning: Podano nieprawidłowe wyrażenie!',
+            color=somsiad.color
+        )
+    else:
+        input_info = (
+            [expression] +
+            [f'{variable[0]} = {variable[1]}' for variable in variables_dict.items()]
+        )
+        input_info_string = '\n'.join(input_info)
+        try:
+            result = parsed_expression.evaluate(variables_dict)
+        except Exception:
             try:
-                variables_dict[key_value_pair[0].strip()] = float(key_value_pair[1].strip())
-            except (IndexError, ValueError):
+                result = parsed_expression.simplify(variables_dict).toString()
+            except Exception:
                 embed = discord.Embed(
-                    title=':warning: Podano nieprawidłowe zmienne!',
+                    title=':warning: Podano nieprawidłowe wyrażenie lub zmienne!',
                     color=somsiad.color
                 )
-                return await ctx.send(ctx.author.mention, embed=embed)
-
-        try:
-            parsed_expression = parser.parse(expression)
-            input_info = (
-                [expression] +
-                [f'{variable[0]} = {variable[1]}' for variable in variables_dict.items()]
-            )
-            input_info_string = '\n'.join(input_info)
-            try:
-                result = parsed_expression.evaluate(variables_dict)
+            else:
                 embed = discord.Embed(
-                    title=':1234: Obliczono wartość wyrażenia',
+                    title=':1234: Uproszczono wyrażenie',
                     color=somsiad.color
                 )
                 embed.add_field(name='Wejście', value=input_info_string, inline=False)
-                embed.add_field(name='Wyjście', value=str(result), inline=False)
-            except Exception:
-                try:
-                    result = parsed_expression.simplify(variables_dict).toString()
-                    embed = discord.Embed(
-                        title=':1234: Uproszczono wyrażenie',
-                        color=somsiad.color
-                    )
-                    embed.add_field(name='Wejście', value=input_info_string, inline=False)
-                    embed.add_field(name='Wyjście', value=result.replace('*', '\*'), inline=False)
-                except Exception:
-                    embed = discord.Embed(
-                        title=':warning: Podano nieprawidłowe wyrażenie lub zmienne!',
-                        color=somsiad.color
-                    )
-        except Exception:
+                embed.add_field(name='Wyjście', value=result.replace('*', '\*'), inline=False)
+        else:
             embed = discord.Embed(
-                title=':warning: Podano nieprawidłowe wyrażenie!',
+                title=':1234: Obliczono wartość wyrażenia',
                 color=somsiad.color
             )
-    else:
-        embed = discord.Embed(
-            title=':warning: Nie podano wyrażenia!',
-            color=somsiad.color
-        )
+            embed.add_field(name='Wejście', value=input_info_string, inline=False)
+            embed.add_field(name='Wyjście', value=str(result), inline=False)
 
     return await ctx.send(ctx.author.mention, embed=embed)
+
+@calculate.error
+async def calculate_error(ctx, error):
+    if isinstance(error, discord.ext.commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            title=':warning: Nie podano wyrażenia do obliczenia!',
+            color=somsiad.color
+        )
+        await ctx.send(ctx.author.mention, embed=embed)
