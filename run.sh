1911b60b1aed735f4429c8b52def714dc0470764
Twixes <dev@twixes.com>
2019-04-23 01:35:34 +0200

    Added an alternative way of launching the bot for future profiling use

--- run.sh
+++ run.sh
@@ -26,6 +26,7 @@ then
     pip3 install -q -U -r $(dirname "$BASH_SOURCE")/requirements.txt
     echo Interpretowanie kodu...
     python3 $(dirname "$BASH_SOURCE")/run.py
+    # python3 -m cProfile -s tottime $(dirname "$BASH_SOURCE")/run.py
 else
     echo W systemie nie znaleziono Pythona 3! Somsiad nie może wstać.
 fi
