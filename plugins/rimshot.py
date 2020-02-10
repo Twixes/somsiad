dc4c17a5c61eb27b1b73bed46e3a3e7c693cd91a
Twixes <dev@twixes.com>
2020-02-11 00:22:18 +0100

    Cogify rimshot

--- plugins/rimshot.py
+++ plugins/rimshot.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Twixes
+# Copyright 2018-2020 Twixes
 
 # This file is part of Somsiad - the Polish Discord bot.
 
@@ -17,11 +17,18 @@ from core import somsiad
 from configuration import configuration
 
 
-@somsiad.command(aliases=['badum', 'badumtss'])
-@commands.cooldown(
-    1, configuration['command_cooldown_per_user_in_seconds'], commands.BucketType.user
-)
-async def rimshot(ctx):
-    """Ba dum tss!"""
-    file = discord.File(fp='./data/BA-DUM-TSS.mp3')
-    await somsiad.send(ctx, file=file)
+class Rimshot(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(aliases=['badum', 'badumtss'])
+    @commands.cooldown(
+        1, configuration['command_cooldown_per_user_in_seconds'], commands.BucketType.user
+    )
+    async def rimshot(self, ctx):
+        """Ba dum tss!"""
+        file = discord.File(fp='./data/BA-DUM-TSS.mp3')
+        await self.bot.send(ctx, file=file)
+
+
+somsiad.add_cog(Rimshot(somsiad))
