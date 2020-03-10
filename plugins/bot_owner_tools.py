492a9a8c6c30eebe99083cc53022ef690d5a1361
Twixes <dev@twixes.com>
2020-03-11 00:08:15 +0100

    Remove v2 embed

--- plugins/bot_owner_tools.py
+++ plugins/bot_owner_tools.py
@@ -20,82 +20,6 @@ class BotOwnerTools(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    def v2_embed(self) -> discord.Embed:
-        embed = self.bot.generate_embed(
-            '📢', 'Somsiad 2.0', 'Nowa wersja już tu jest, a w niej wiele nowych funkcji i usprawnień. Poniżej najważniejsze z nich.'
-        )
-        embed.add_field(
-            name='🔧',
-            value='Administratorzy serwera mogą teraz ustawić na nim własny prefiks komend Somsiada z użyciem komendy `prefiks`.\nWięcej informacji pod `prefiks` bez podkomendy.',
-            inline=False
-        )
-        embed.add_field(
-            name='🍅',
-            value='Z nową komendą `przypomnij` bot przypomni o każdej ważnej sprawie.',
-            inline=False
-        )
-        embed.add_field(
-            name='🎥',
-            value='Zamiast [OMDb](https://www.omdbapi.com/) poprzez komendę `omdb` lepsze informacje o filmach i serialach można teraz uzyskać z [TMDb](https://www.themoviedb.org/) poprzez komendę `tmdb`. Dodatkowo dostępne są też informacje o osobach z przemysłu, np. aktorkach bądź reżyserach. W ramach tej funkcji działają również odrębne komendy `film` i `serial`.\nWięcej informacji pod `tmdb` bez podkomendy.',
-            inline=False
-        )
-        embed.add_field(
-            name='🧠',
-            value='Nowa komenda `wolframalpha` pozwala na dostęp do całych zasobów wiedzy i mocy obliczeniowej [Wolfram Alpha](https://www.wolframalpha.com/). Zastępuje komendy `oblicz` i `waluta`.',
-            inline=False
-        )
-        embed.add_field(
-            name='🎂',
-            value='Ulepszona komenda `urodziny` zapamiętuje teraz datę urodzin globalnie z upublicznianiem jej osobno na każdym serwerze. Dodatkowo administratorzy serwera mogą z użyciem `urodziny powiadomienia` włączyć na wybranym kanale automatyczne powiadomienia o urodzinach użytkowników serwera, wraz z życzeniami. Z powodu tak dużych zmian w działaniu funkcji daty urodzin zostały w aktualizacji zresetowane.\nWięcej informacji pod `urodziny` bez podkomendy.',
-            inline=False
-        )
-        embed.add_field(
-            name='👽',
-            value='Całkowicie przepisane komendy `r` i `u` oprócz linku dostarczają teraz też informacje o znalezionym, odpowiednio, subreddicie lub użytkowniku Reddita.',
-            inline=False
-        )
-        embed.add_field(
-            name='🎓',
-            value='Do `ilejeszcze` dodano podkomendę `matura` odliczającą do pierwszego dnia matur.',
-            inline=False
-        )
-        embed.add_field(
-            name='🎨',
-            value='Z nową komendą `kolory` użytkownicy mogą sami wybierać sobie kolor nicku spośród utworzonych przez administrację serwera specjalnych ról.\nWięcej informacji pod `kolory` bez podkomendy.',
-            inline=False
-        )
-        embed.add_field(
-            name='✍️',
-            value='Nowa komenda moderacyjna `przebacz` pozwala administratorom serwera usunąć ostrzeżenia z kartoteki użytkownika.',
-            inline=False
-        )
-        embed.add_field(
-            name='🇫',
-            value='Do komendy własnych reakcji `reaguj` dodano obsługę spacji (w ich miejsca wstawiane są losowe emoji) oraz serwerowych emoji.',
-            inline=False
-        )
-        embed.add_field(
-            name='📈',
-            value='Do raportów aktywności komendy `stat` dodano nowe informacje i wykresy, a także znacznie usprawniono ich generowanie.',
-            inline=False
-        )
-        embed.add_field(
-            name='🖼',
-            value='Dodano komendę obracania obrazków `obróć` i ulepszono `deepfry`.',
-            inline=False
-        )
-        embed.add_field(
-            name='🔄',
-            value='Przepisano komendy `głosowanie` i `spal` tak, by wykonywały się również po restarcie bota.',
-            inline=False
-        )
-        embed.add_field(
-            name='⚠️',
-            value='Dodano rejestrowanie błędów i okoliczności ich zajścia, co będzie ułatwiać ich naprawę. Błędy można do tego zgłaszać na [serwerze Somsiad Labs](http://discord.gg/xRCpDs7). Mile widziane również sugestie nowych funkcji i innych usprawnień.',
-            inline=False
-        )
-        return embed
-
     @commands.command(aliases=['wejdź'])
     @cooldown()
     @commands.is_owner()
@@ -127,21 +51,15 @@ class BotOwnerTools(commands.Cog):
     @commands.is_owner()
     async def announce_globally(self, ctx, *, raw_announcement):
         """Makes an announcement on all servers smaller than 10000 members not containing "bot" in their name."""
-        if raw_announcement == 'v2':
-            embed = self.v2_embed()
+        announcement = raw_announcement.replace('\\n','\n').strip(';').split(';')
+        if announcement[0].startswith('!'):
+            description = announcement[0].lstrip('!').strip()
+            announcement = announcement[1:]
         else:
-            announcement = raw_announcement.replace('\\n','\n').strip(';').split(';')
-            if announcement[0].startswith('!'):
-                description = announcement[0].lstrip('!').strip()
-                announcement = announcement[1:]
-            else:
-                description = None
-
-            embed = self.bot.generate_embed('📢', 'Ogłoszenie somsiedzkie', description)
-
-            for n in range(0, len(announcement) - 1, 2):
-                embed.add_field(name=announcement[n].strip(), value=announcement[n+1].strip(), inline=False)
-
+            description = None
+        embed = self.bot.generate_embed('📢', 'Ogłoszenie somsiedzkie', description)
+        for n in range(0, len(announcement) - 1, 2):
+            embed.add_field(name=announcement[n].strip(), value=announcement[n+1].strip(), inline=False)
         for server in ctx.bot.guilds:
             if not server.name or 'bot' not in server.name.lower():
                 for channel in server.text_channels:
@@ -159,21 +77,17 @@ class BotOwnerTools(commands.Cog):
     @commands.guild_only()
     async def announce_locally(self, ctx, *, raw_announcement):
         """Makes an announcement only on the server where the command was invoked."""
-        if raw_announcement == 'v2':
-            embed = self.v2_embed()
+        announcement = raw_announcement.replace('\\n','\n').strip(';').split(';')
+        if announcement[0].startswith('!'):
+            description = announcement[0].lstrip('!').strip()
+            announcement = announcement[1:]
         else:
-            announcement = raw_announcement.replace('\\n','\n').strip(';').split(';')
-            if announcement[0].startswith('!'):
-                description = announcement[0].lstrip('!').strip()
-                announcement = announcement[1:]
-            else:
-                description = None
+            description = None
 
-            embed = self.bot.generate_embed('📢', 'Ogłoszenie somsiedzkie', description)
+        embed = self.bot.generate_embed('📢', 'Ogłoszenie somsiedzkie', description)
 
-            for n in range(0, len(announcement) - 1, 2):
-                embed.add_field(name=announcement[n].strip(), value=announcement[n+1].strip(), inline=False)
-
+        for n in range(0, len(announcement) - 1, 2):
+            embed.add_field(name=announcement[n].strip(), value=announcement[n+1].strip(), inline=False)
         for channel in ctx.guild.text_channels:
             if channel.permissions_for(ctx.me).send_messages:
                 await channel.send(embed=embed)
