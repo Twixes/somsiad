790916d2fd3af41f409287a1e6999941342986ad
Michael Matloka <dev@twixes.com>
2021-05-03 00:13:24 +0200

    Add Twitter attachment count reacting

--- configuration.py
+++ configuration.py
@@ -118,8 +118,10 @@ SETTINGS = (
     Setting('last_fm_key', description='Klucz API Last.fm', optional=True),
     Setting(
         'yandex_translate_key',
-        description='Klucz API Yandex Translate', optional=True,
+        description='Klucz API Yandex Translate',
+        optional=True,
     ),
+    Setting('twitter_bearer_token', description='Token aplikacji Twittera', optional=True),
     Setting(
         'disco_max_file_size_in_mib', description='Maksymalny rozmiar pliku utworu disco', unit='MiB', default_value=16
     ),
---
+++ plugins/twitter.py
@@ -0,0 +1,69 @@
+# Copyright 2021 Twixes
+
+# This file is part of Somsiad - the Polish Discord bot.
+
+# Somsiad is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# Somsiad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with Somsiad.
+# If not, see <https://www.gnu.org/licenses/>.
+
+import re
+from dataclasses import dataclass, field
+from typing import Any, Dict, Optional, cast
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+from configuration import configuration
+from somsiad import Somsiad
+
+from .reactions import React
+
+
+@dataclass
+class Tweet:
+    id: str
+    text: str = field(default_factory=str)
+    attachments: Dict[str, Any] = field(default_factory=dict)
+
+
+async def fetch_tweet_with_attachments(tweet_id: str, session: aiohttp.ClientSession) -> Optional[Tweet]:
+    url = f"https://api.twitter.com/2/tweets?ids={tweet_id}&tweet.fields=attachments"
+    headers = {"Authorization": f"Bearer {configuration['twitter_bearer_token']}"}
+    response = await session.request("GET", url, headers=headers)
+    if response.status != 200:
+        raise Exception(f"Twitter API request returned an error: {response.status} {await response.text()}")
+    response_parsed = await response.json()
+    if response_parsed.get('errors'):
+        if any((error['title'] != 'Not Found Error' for error in response_parsed['errors'])):
+            raise Exception(f"Twitter API request returned an error: {response_parsed}")
+        return None
+    return Tweet(**response_parsed['data'][0])
+
+
+class Twitter(commands.Cog):
+    TWEET_URL_REGEX = re.compile(r'https?:\/\/(?:(?:m|www)\.)?twitter\.com\/\w+\/status\/(\d+)\b', flags=re.IGNORECASE)
+
+    def __init__(self, bot: Somsiad):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        matches = self.TWEET_URL_REGEX.finditer(message.content)
+        for match in matches:
+            tweet_id = match.group(1)
+            tweet = await fetch_tweet_with_attachments(tweet_id, cast(aiohttp.ClientSession, self.bot.session))
+            if tweet is not None and tweet.attachments and tweet.attachments['media_keys']:
+                attachment_count = len(tweet.attachments['media_keys'])
+                if attachment_count > 1:
+                    await message.add_reaction(React.EMOJIS[0][str(attachment_count)])
+
+
+def setup(bot: Somsiad):
+    if configuration.get('twitter_bearer_token') is not None:
+        bot.add_cog(Twitter(bot))
