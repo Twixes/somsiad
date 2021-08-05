a97fe077cf824d7358e355c15a40af748f2880ed
Michael Matloka <dev@twixes.com>
2021-08-05 22:40:34 +0200

    Add 8-ball inversion

--- plugins/eightball.py
+++ plugins/eightball.py
@@ -14,12 +14,14 @@
 import datetime as dt
 import hashlib
 import random
+from typing import Optional, cast
+
+import discord
 from somsiad import SomsiadMixin
 
 from discord.ext import commands
 
 from core import cooldown
-
 
 class Eightball(commands.Cog, SomsiadMixin):
     CATEGORIES_POOL = ['affirmative'] * 7 + ['negative'] * 7 + ['enigmatic'] * 1
@@ -49,8 +51,8 @@ class Eightball(commands.Cog, SomsiadMixin):
             'Raczej nie.',
             'Nie wydaje mi się.',
             'Mój wywiad donosi: NIE.',
-            'Nie, nie i jeszcze raz NIE.'
-            'Sprawdziłem wszystkie dostępne mi zasoby wiedzy i wygląda na to, że nie.'
+            'Nie, nie i jeszcze raz NIE.',
+            'Sprawdziłem wszystkie dostępne mi zasoby wiedzy i wygląda na to, że nie.',
             'Mocno skłaniam się ku: nie.',
             'Nope.',
             'Nein!',
@@ -72,11 +74,12 @@ class Eightball(commands.Cog, SomsiadMixin):
         ],
     }
 
-    def ask(self, stripped_question: str) -> str:
+    def ask(self, stripped_question: str, category: Optional[str] = None) -> str:
         question_hash = hashlib.md5(stripped_question.encode())
         question_hash.update(dt.date.today().isoformat().encode())
         question_hash_int = int.from_bytes(question_hash.digest(), 'big')
-        category = self.CATEGORIES_POOL[question_hash_int % len(self.CATEGORIES_POOL)]
+        if not category:
+            category = self.CATEGORIES_POOL[question_hash_int % len(self.CATEGORIES_POOL)]
         answer = self.ANSWERS[category][question_hash_int % len(self.ANSWERS)]
         return answer
 
@@ -86,7 +89,7 @@ class Eightball(commands.Cog, SomsiadMixin):
 
     @commands.command(aliases=['8ball', '8-ball', '8', 'czy'])
     @cooldown()
-    async def eightball(self, ctx, *, question: commands.clean_content(fix_channel_mentions=True) = ''):
+    async def eightball(self, ctx: commands.Context, *, question: commands.clean_content(fix_channel_mentions=True) = ''):
         """Returns an 8-Ball answer."""
         stripped_question = question.strip('`~!@#$%^&*()-_=+[{]}\\|;:\'",<.>/?‽').lower()
         if stripped_question:
@@ -97,6 +100,28 @@ class Eightball(commands.Cog, SomsiadMixin):
         else:
             text = '⚠️ By zadać magicznej kuli pytanie musisz użyć *słów*.'
         await self.bot.send(ctx, text)
+
+    @commands.command(aliases=['niepierdol'])
+    @cooldown()
+    async def dontbullshit(self, ctx: commands.Context):
+        """Inverts the last 8-Ball answer in the channel."""
+        async for message in ctx.history(limit=10):
+            if message.author != ctx.me:
+                continue
+            message_content = cast(str, cast(discord.Message, message).clean_content)
+            if not message_content.startswith('🎱 '):
+                continue
+            previous_answer = message_content[2:]
+            if previous_answer in self.ANSWERS['affirmative']:
+                new_category = 'negative'
+            elif previous_answer in self.ANSWERS['negative']:
+                new_category = 'affirmative'
+            else:
+                new_category = random.choice(['affirmative', 'negative'])
+            new_text = f'🎱 {random.choice(self.ANSWERS[new_category])}'
+            await message.edit(content=new_text)
+            break
+
 
 
 def setup(bot: commands.Bot):
