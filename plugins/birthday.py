6e2cae6a7a271b44dbd1dce6cac12219d49d4a7f
Michael Matloka <dev@twixes.com>
2022-04-22 01:26:00 +0200

    Fix property access on None

--- plugins/birthday.py
+++ plugins/birthday.py
@@ -257,7 +257,7 @@ class Birthday(commands.Cog):
                     )
                     extra = f'{these_servers_number_form} otrzymasz używającej tej komendy w wiadomościach prywatnych.'
             else:
-                names = [self.bot.get_guild(server.id).name for server in born_person.birthday_public_servers]
+                names = [guild.name for guild in (self.bot.get_guild(server.id) for server in born_person.birthday_public_servers) if guild is not None]
                 servers_number_form = word_number_form(
                     number_of_other_servers, 'serwerze', 'serwerach', include_number=False
                 )
