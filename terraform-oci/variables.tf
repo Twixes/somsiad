107dfa7801067d68b6870af02d06c0787d56f246
Michael Matloka <dev@twixes.com>
2022-12-30 16:44:01 +0100

    Improve `variables.tf` formatting

--- terraform-oci/variables.tf
+++ terraform-oci/variables.tf
@@ -37,9 +37,9 @@ variable "compartment_ocid" {
 }
 
 variable "node_source_image_ocid" {
-  description = "OCI image OCID from https://docs.oracle.com/en-us/iaas/Content/ContEng/Reference/contengimagesshapes.htm#images__oke-images"
+  description = "OCI image ID, see https://docs.oracle.com/en-us/iaas/Content/ContEng/Reference/contengimagesshapes.htm#images__oke-images"
   type        = string
   nullable    = false
-  # OKE Oracle Linux 8.6, obtained via `oci ce node-pool-options get --node-pool-option-id all`
   default     = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaisg5jmxrefsgaexzap4mm2spbenrjmeem6aoxescosf6wu7h4nuq"
+  # Above is OKE ARM Oracle Linux 8.6 for K8s 1.24.1, via `oci ce node-pool-options get --node-pool-option-id all`
 }
