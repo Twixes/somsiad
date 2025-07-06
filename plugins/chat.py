4900df9bdddcfdb1e9f75385783264243e380bf2
Michael Matloka <michael@matloka.com>
2025-07-06 19:34:07 +0200

    Clarify internet access more

--- plugins/chat.py
+++ plugins/chat.py
@@ -118,7 +118,7 @@ A "JP2GMD" to mem oznaczający "Jan Paweł II Gwałcił Małe Dzieci". Inna śmi
 Poza czatem udostępniasz jako bot różne komendy, których spis dostępny jest pod komendą `{{command_prefix}}pomocy`.
 Masz dostęp do wszystkich swoich komend w ramach tool calling. Jeśli pomożesz użytkownikowi lepiej poprzez wykonanie komendy, zawsze to zrób.
 Możesz wywoływać komendy wielokrotnie by wykonywać prośby i uzyskiwać informacje.
-W razie potrzeby informacji o świecie (aktualnościach, statystykach, itp.), użyj narzędzia `{ASK_ONLINE_FUNCTION_DEFINITION.name}`.
+W razie potrzeby informacji o świecie (aktualnościach, statystykach, itp.), użyj narzędzia `{ASK_ONLINE_FUNCTION_DEFINITION.name}`. Daje ono pełny dostęp do internetu.
 W szczególności wykonywaj zapytania internetowe by potwierdzać czy coś jest prawdą/fałszem, oraz gdy masz coś sprawdzić na jakiejś stronie internetowej.
 Wykorzystaj informacje z `{ASK_ONLINE_FUNCTION_DEFINITION.name}` jako źródła dla właściwej odpowiedzi. Nigdy nie każ użytkownikowi samemu użyć Google.
 Na prośbę stworzenia ankiety, musisz użyć narzędzia `ankieta` - pamiętaj podać czas trwania lub zakończenia jako pierwszy argument.
